6111abfe987dd8308761e1b28dcb11d573d2328e
Sarthak Singhaniya <sarthaksinghaniya789@gmail.com>
2026-07-16 22:23:21 +0530

    Time: 3 ms (65.42%), Space: 47.5 MB (77.8%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int duplicate = -1;
+        int missing = -1;
+
+        // Step 1: Find duplicate
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+
+            if (nums[index] < 0) {
+                duplicate = Math.abs(nums[i]);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+
+        // Step 2: Find missing
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                missing = i + 1;
+                break;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
