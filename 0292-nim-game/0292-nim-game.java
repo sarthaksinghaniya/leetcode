21451856031dbc1b45d573876909cfb2ac1c1bf8
Sarthak Singhaniya <sarthaksinghaniya789@gmail.com>
2026-08-17 19:57:07 +0530

    Time: 0 ms (100%), Space: 42.2 MB (35.54%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if (n % 4 == 0) {
-            return false;
+        if(n %4 == 0){
+            return false ;
         }
-        return true;
+        return true ;
     }
 }
